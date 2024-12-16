6fa39cdfcf45c9d49a570e39ba225ae5902d4250
momomozakiki <62495487+momomozakiki@users.noreply.github.com>
2024-12-16 23:29:59 +0800

    update the webpack.config for devserver

--- webpack.config.js
+++ webpack.config.js
@@ -25,4 +25,12 @@ module.exports = {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
+  devtool: 'source-map',  // This will generate a source map for easier debugging
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),  // Directory to serve static files from
+    compress: true,  // Enable gzip compression
+    port: 9000,  // You can change the port to any other port you prefer
+    open: true,  // Automatically open the browser when the dev server starts
+  },
 };
